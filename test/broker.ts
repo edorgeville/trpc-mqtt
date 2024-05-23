2ac5a8d9995c4f8119b8d942fb930cb4f7e6b522
Erwan d'Orgeville <info@erwandorgeville.com>
2024-05-23 10:34:22 -0400

    fix: remove unused file

--- test/broker.ts
+++
@@ -1,9 +0,0 @@
-import Aedes from 'aedes';
-import { createServer } from 'net';
-
-const aedes = new Aedes();
-const server = createServer(aedes.handle);
-
-server.listen(1883, () => {
-  console.log('server started and listening on port 1883');
-});
